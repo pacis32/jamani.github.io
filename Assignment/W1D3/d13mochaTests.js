cca96700c0436736596d87cfa3d5636023c18dd7
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-07 20:22:25 -0500

    update

--- Assignment/W1D3/d13mochaTests.js
+++ Assignment/W1D3/d13mochaTests.js
@@ -5,12 +5,15 @@ const functionsModule = require("./functions.js");
 const isVowel = functionsModule.isVowel;
 const computeSalesCommission= functionsModule.computeSalesCommission;
 const compoundInterest= functionsModule.compoundInterest;
+const  calcDownpayment= functionsModule.calcDownpayment;
+const sumDigits =  functionsModule.sumDigits;
+const multDigits = functionsModule.multDigits;
+const convertFahrenheit = functionsModule.convertFahrenheit; 
+const alcDistance = functionsModule.alcDistance;
 
-module.exports = {isVowel, computeSalesCommission,compoundInterest };
 
-const { it } = require("node:test");
+const assert= require("assert");
 
-const assert= require('assert');
 
 describe("isVowel", function () {
     it("a is vowel", function () {
@@ -57,16 +60,86 @@ describe("isVowel", function () {
     });
    });
 
-   describer("test of compoundInterest", function(){
+   describe("test of compoundInterest", function(){
     it("test initial amount 100, interest rate 10, and number of Years to compound 1", function(){
-        assert.strictEqual(compoundInterest(100, 10,1),110.47)
+        assert.equal(compoundInterest(100, 10,1),110.47);
     });
     it("test initial amount 100000, interest rate 5, and number of Years to compound 10 ", function(){
-        assert.strictEqual(compoundInterest(100000, 5,10),16470.09)
+        assert.equal(compoundInterest(10000, 5,10),16470.09);
+    });
+   }); 
+
+   describe("test of calcDownpayment", function(){
+    it("test cost ", function(){
+        assert.equal(calcDownpayment(40000),2000);
+    });
+ 
+    it("test cost ", function(){
+        assert.equal(calcDownpayment(50000),2500);
     });
 
+    it("test cost ", function(){
+        assert.equal(calcDownpayment(100000),7500);
+    });
+ 
+    it("test cost ", function(){
+        assert.equal(calcDownpayment(250000),25000);
+    });
 
    });
 
+   describe("test sumdigits", function(){
+    it(" test the sum of the digits of a  given number",function(){
+        assert.equal(sumDigits(1234),10);
+    });
+    it(" test the sum of the digits of a  given number",function(){
+        assert.equal(sumDigits(102),3);
+    });
 
+    it(" test the sum of the digits of a  given number",function(){
+        assert.equal(sumDigits(8),8);
+    });
+
+   });
+
+   describe("test the multiplication of digit of a number", function(){
+    it(" test the product of the digits of a  given number",function(){
+        assert.equal(multDigits(1234),24);
+    });
+    it(" test the product of the digits of a  given number",function(){
+        assert.equal(multDigits(102),0);
+    });
+
+    it(" test the product of the digits of a  given number",function(){
+        assert.equal(multDigits(8),8);
+    });
+
+   });
+
+   describe("test conversion from Fahrenheit to celcuis  ", function(){
+    it(" test the conversion of fahrenheit to celcius degrees",function(){
+        assert.equal(convertFahrenheit(32),0);
+    });
+    it(" test the conversion of farhrenheit  to celcius degrees",function(){
+        assert.equal(convertFahrenheit(0),-17.7778);
+    });
+
+    it(" test the conversion of farhrenheit to celcius degrees",function(){
+        assert.equal(convertFahrenheit(212),100);
+    });
+
+   });
+
+   describe("test calcDistance ", function(){
+    it(" test co-ordinates of two points into the distance btn these two points",function(){
+        assert.equal(alcDistance(0,0,5,5),7.07);
+    });
+    
+   });
+
+
+
+
+
+  
   
