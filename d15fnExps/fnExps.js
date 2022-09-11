325373912194b84fe2a96fc197dd23898fbc8e66
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-11 16:06:39 -0500

    update

--- d15fnExps/fnExps.js
+++ d15fnExps/fnExps.js
@@ -7,21 +7,43 @@
 module.exports = {double, times100, myMap }; //add all of your function names here that you need for the node mocha tests
 
 /**
- *@returns {number} double the input
+ * 
+ * @param {number} number - any number times 2
+ * @returns {number} double the input
  */
-let double = function(number){
-    return number*2;
-
+    function double(number){
+   return number*2;
 }
-/**
- * @returns {number} 100 times the input
- */
- function times100(){
- }
-
 
 /**
- * @returns {Array} creates a new array with function mapped to each element
+ * 
+ * @param {number} number - any number times 100
+ * @returns  {number} -100 times the input
  */
-function myMap(){
+  function times100(number){
+   return  number*100;
+ 
+  }
+
+/**
+ * 
+ * @param {arr} arr- array of numbers 
+ * @param {function} function - any function that takes a number as paramater
+ * @returns  {Array} creates a new array with function mapped to each element
+ */
+   function myMap(arr, anyfunction){
+   let arr1=[];
+   for( let element of arr){
+    arr1.push(anyfunction(element));
+   }
+
+   return arr1;
 }
+
+
+
+
+
+
+
+
