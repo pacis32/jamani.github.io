0a80a0324e8dfde87ab14a1800ffaa765b295b94
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-11 11:31:16 -0500

    update

---
+++ d15fnExps/fnExps.js
@@ -0,0 +1,27 @@
+
+
+"use strict";
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
+*/ 
+module.exports = {double, times100, myMap }; //add all of your function names here that you need for the node mocha tests
+
+/**
+ *@returns {number} double the input
+ */
+let double = function(number){
+    return number*2;
+
+}
+/**
+ * @returns {number} 100 times the input
+ */
+ function times100(){
+ }
+
+
+/**
+ * @returns {Array} creates a new array with function mapped to each element
+ */
+function myMap(){
+}
