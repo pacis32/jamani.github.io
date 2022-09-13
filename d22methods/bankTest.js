493f3e45a92f526e4d2a501d3a61ca06b7135114
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-13 17:24:18 -0500

    update# Please enter the commit message for your changes. Lines starting

--- d22methods/bankTest.js
+++ d22methods/bankTest.js
@@ -2,10 +2,10 @@
 /* global assert bank */
 
 /* comment out the following for the browser */
-const assert = require("assert");  //always need this with node
+/*const assert = require("assert");  //always need this with node
 const myExports = require("./bank.js");  //with node need the name of your file with your functions here
 const bank = myExports.bank;  //do this for all of the functions used in the Mocha tests
-
+*/
 describe("bank tests", function () {
 
     it("checks initial balance", function () {
