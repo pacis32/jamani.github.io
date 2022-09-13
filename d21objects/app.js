4bff1fac41a7e6945d15cd18dccc38a38bae0f03
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-12 21:07:06 -0500

    update

--- d21objects/app.js
+++ d21objects/app.js
@@ -1,31 +1,26 @@
-/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-//module.exports = { findTitles, findAuthors, findIDs, createBook}; //add all of your function names here that you need for the node mocha tests
 
-/*
-o	createBook, which will take title, author, and libraryID as inputs.  It will create a new book object and add it to the library, 
-which will be represented as a global array named libraryBooks.  createBook should return the newly created book.
-o	findTitles, which will find all the book titles in libraryBooks and return them in an alphabetically ordered array.
-o	findAuthors, which will find all the authors in libraryBooks and return them in an alphabetically ordered array.
-o	findIDs, which will find all the libraryIDs in libraryBooks and return them in an alphabetically ordered array.
-*/
+
+"use strict";
+/* eslint-disable*/
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+//module.exports = {findTitles, findAuthors, findIDs, createBook }; //add all of your function names here that you need for the node mocha tests
+
 
 let library = [
     { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
     { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
     { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3245 }
-];
+]; 
 
 /**
  * Event handler to display library titles sorted alphabetically
- * @returns {undefined}
+ *@returns {string} the string of tittles;
  */
 function showTitles() {
 
-    /* put all titles into an array, then sort, then join with newline and insert in textarea innerHTML */
 
     const titles = findTitles();
 
-    /*need to sort and then join the titles still (e.g., someArray.join("\n")  */
     titles.sort();
     const titleString = titles.join("\n");
 
@@ -33,16 +28,54 @@ function showTitles() {
     textArea.innerHTML = titleString;
 }
 
+
+/**
+ * Event handler to display library authors sorted alphabetically
+ * @returns {string} the string of authors;
+ */
+ function showAuthors() {
+
+
+    const authors = findAuthors();
+
+    authors.sort();
+    const authorsString = authors.join("\n");
+
+    let textArea = document.getElementById("displayArea");
+    textArea.innerHTML = authorsString;
+
+    
+}
+
+/**
+ * Event handler to display library ids sorted alphabetically
+ * @returns {string} the string of ids;
+ */
+ function showIds() {
+
+
+    const ids = findIDs();
+
+    ids.sort();
+    const idsString = ids.join("\n");
+
+    let textArea = document.getElementById("displayArea");
+    textArea.innerHTML = idsString;
+}
+
+
+
 /**
  * 
  * @return {object} array holding all titles as elements
  */
 function findTitles() {
     let titles = [];
-
-
-    titles = ["Mockingjay: The Final Book of The Hunger Games", "The Road Ahead", "Walter Isaacson"];  //FIX THIS!!
-    // implement this and other functions
+   
+    for( let elem of library){
+        titles.push(elem.title);
+    }
+   titles.sort();
     return titles;
 }
 
@@ -50,14 +83,16 @@ function findTitles() {
  * @returns {undefined} no return
  * Event handler for Add book button.  Creates and adds book to the library
  */
-function createBook(title,author,libraryID) {
-    const title = document.getElementById("title"); //retrieves the book title from the title textbox
-    console.log("title is: ", title.value);
-    alert("title:  " + title.value);
+function createBook(title, author,libraryID){
 
-    //const newID = library.length + 5000;  // hack to get a unique id for now
-    //finish the implementation -- get the author, create a book object, and add to the library array
-    let newBook = {title, author, libraryID };
+    //title = document.getElementById("title").value; //retrieves the book title from the title textbox
+    //author = document.getElementById("author").value;
+   //libraryID = document.getElementById("libids").value;
+   //finish the implementation -- get the author, create a book object, and add to the library array
+
+
+
+   let newBook = {title, author,libraryID};
     
     library.push(newBook);
     
@@ -95,4 +130,19 @@ ids.sort();
 return ids;
 }
 
+
+/**
+ * Event handler to display library authors sorted alphabetically
+ * @returns {string} the sorted words;
+ */
+ function scramble() {
+
+
+    const titles = findTitles();
+
+    const titleString = titles.join(" ").toString().split(" ").sort((aaa,bbb) => (aaa.length > bbb.length)? 1 : -1).join("\n");
+
+    let textArea = document.getElementById("displayArea"); 
+    textArea.innerHTML = titleString;
+}
 
