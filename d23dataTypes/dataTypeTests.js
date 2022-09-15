7a0c569b1076fa269697df8314f0a8dd4f2215a5
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-14 21:04:07 -0500

    update

--- d23dataTypes/dataTypeTests.js
+++ d23dataTypes/dataTypeTests.js
@@ -1,7 +1,7 @@
 "use strict";
 /* global assert  ucFirst checkSpam truncate extractCurrencyValue camelize  */
 /* comment out the node specific code when going to the browser*/
-const assert = require("assert");  //always need this with node
+/*const assert = require("assert");  //always need this with node
 const myExports = require("./app.js");  //with node need the name of your file with your functions here
 const getMaxSubSum = myExports.getMaxSubSum;  //do this for all of the functions used in the Mocha tests
 const ucFirst = myExports.ucFirst; 
@@ -9,7 +9,7 @@ const truncate = myExports.truncate;
 const camelize = myExports.camelize; 
 const checkSpam= myExports.checkSpam;
 const extractCurrencyValue=myExports.extractCurrencyValue;
-
+*/
  
 /* eslint-disable quotes */
 describe("ucFirst", function() {
