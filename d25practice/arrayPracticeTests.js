f6ae64299f9d6155dab7c2e13e900ef9545f49fb
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-18 20:37:13 -0500

    updating

--- d25practice/arrayPracticeTests.js
+++ d25practice/arrayPracticeTests.js
@@ -3,7 +3,7 @@
 /* global assert doubleNums  doubleAges filterEven filterOver10 findEvenNum findEvenAge  includesEvenAge findSum findAverage findMax findMaxAges  findAverageEven findAverageOdd*/
 /* comment out the node specific code when going to the browser*/
 
-const assert = require("assert");  //always need this with node
+/*const assert = require("assert");  //always need this with node
 const myExports = require("./arrayPractice.js");  //with node need the name of your file with your functions here
 const doubleNums = myExports.doubleNums;  //do this for all of the functions used in the Mocha tests
 const doubleAges = myExports.doubleAges;
@@ -18,6 +18,7 @@ const findMax = myExports.findMax;
 const findMaxAges = myExports.findMaxAges;
 const findAverageEven = myExports.findAverageEven;
 const findAverageOdd = myExports.findAverageOdd;
+*/
 
 
 let numArray;
