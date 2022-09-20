510986ae337523821d7fe1051e8c98d4459894b1
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-19 20:35:59 -0500

    updating

--- d31destrucure/destructure.js
+++ d31destrucure/destructure.js
@@ -1,45 +1,41 @@
 //"use strict";
 /* eslint-disable */
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-//module.exports = { topSalary }; //add all of your function names here that you need for the node mocha tests
+module.exports = { topSalary,assignment,cloneObject }; //add all of your function names here that you need for the node mocha tests
 
 
-function topSalary(salaries) {
-
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+function assignment({ name, years, isAdmin = false }) {
+  // console.log(name);
+  // console.log(years);
+  // console.log(isAdmin);
+  return `${name}+${years}+${isAdmin}`;
 }
 
-ccc = 10;
-console.log(alpha);
 
 
-
-function assignment({ name, years, isAdmin = false }) {
-    // console.log(name);
-    // console.log(years);
-    // console.log(isAdmin);
-    return `${name}+${years}+${isAdmin}`;
-  }
-  
-  
-  
-  function topSalary(obj) {
-    let highSalArr = [0, 0];
-    if (Object.entries(obj).length > 0) {
-      for (let [key, value] of Object.entries(obj)) {
-        if ([value] > highSalArr[1]) {
-          highSalArr = [key, value];
-        }
+function topSalary(obj) {
+  let highSalArr = [0, 0];
+  if (Object.entries(obj).length > 0) {
+    for (let [key, value] of Object.entries(obj)) {
+      if ([value] > highSalArr[1]) {
+        highSalArr = [key, value];
       }
-      return highSalArr[0];
-    } else {
-      return null;
     }
+    return highSalArr[0];
+  } else {
+    return null;
   }
-  
-  
-  function cloneObject(obj) {
-    let jsonObject = JSON.stringify(obj);
-    let newObject = JSON.parse(jsonObject);
-    console.log(newObject);
-    return newObject === obj;
-  }
+}
+
+
+function cloneObject(obj) {
+  let jsonObject = JSON.stringify(obj);
+  let newObject = JSON.parse(jsonObject);
+  console.log(newObject);
+  return newObject === obj;
+}
--- d31destrucure/destructureTests.js
+++ d31destrucure/destructureTests.js
@@ -2,11 +2,12 @@
  /* global assert topSalary   */
 /* eslint-disable quotes   */  
 
-/* comment out the node specific code when going to the browser
+// comment out the node specific code when going to the browser
 const assert = require("assert");
 const myExports = require("./destructure.js");
 const topSalary = myExports.topSalary;  //do this for all of the functions used in the Mocha tests
-*/
+const assignment= myExports.assignment;
+const cloneObject= myExports.cloneObject;
  
     
 /* 
@@ -15,19 +16,39 @@ Create the function topSalary(salaries) that returns the name of the top-paid pe
     If there are multiple top-paid persons, return any of them.
 P.S. Use Object.entries and destructuring to iterate over key/value pairs.
 */
-describe("topSalary", function() {
-    it("returns top-paid person", function() {
-      let salaries = {
-        "John": 100,
-        "Pete": 300,
-        "Mary": 250
-      };
-  
-      assert.equal( topSalary(salaries), "Pete" );
-    });
-  
-    it("returns null for the empty object", function() {
-        console.log("topsalary: ", topSalary({}) );
-      assert.strictEqual( topSalary({}), null);
-    });
+describe("topSalary", function () {
+  it("returns top-paid person", function () {
+    let salaries = {
+      "John": 100,
+      "Pete": 300,
+      "Mary": 250
+    };
+
+    assert.equal(topSalary(salaries), "Pete");
   });
+
+  it("returns null for the empty object", function () {
+    console.log("topsalary: ", topSalary({}));
+    assert.strictEqual(topSalary({}), null);
+  });
+});
+
+describe("destructuring assignment", function () {
+  it("returns destructured object into variables", function () {
+    let user = {
+      name: "Tembo",
+      years: 38
+    };
+    assert.equal(assignment(user), "John+30+false");
+  });
+});
+
+describe("clones object using JSON", function () {
+  it("checks strict equality of given object and clone object", function () {
+    let user = {
+      name: "Akanje Jimmy",
+      age: 28
+    };
+    assert.equal(cloneObject(user), false);
+  });
+});
