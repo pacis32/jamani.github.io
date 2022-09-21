4b4048c369a1209b65b029498a5495fd6d2346e1
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-20 20:31:12 -0500

    update

--- Assignment/Inclass/test.js
+++ Assignment/Inclass/test.js
@@ -1,60 +1,11 @@
-"use strict";
-/*eslint-disable */
-/**
- * 
- * @param {number} num 
- * @param {*number} poer 
- */
+const arr1234 = [1, 2, 3, 4, 5];
+const [a, b, ...c] = arr1234;
+console.log(a);
+console.log(b);
+console.log(c);
 
-
- /*function findLongestWord(arr){
-   let longest=arr[0];
-   for(let i=0; i<arr.length; i++){
-
-       if(arr[i].length < longest.length){
-           max=arr[i];
-       }
-       return longest;
-   }
-   
-}
-
-
-function longestword (str){
-   if(str == undefined || str == null)
-      return null;
-   var w = str.split(" ");
-   var result = null;
-   let count = 0;
-   for(var i = 0; i < w.lenght; i++){
-      if(w[i].length > count){
-          count = w[i].lenght;
-          result = w[i];
-      }
-   }
-   return result;
-}
-let r =longestWord(["amani", "amatama","huku"]);
-console.log(r);
-
-
-function countElement(arr){
-   let count=0;
-   for(let i=0; i<arr.length;i++){
-      count=count+arr[i];
-    console.log(arr[i]);
-   }
-   return count;
-}
-*/
-
-
-// /* write a function, sumPoints, to get the total points across all players */
-
-const player1 = {name: "Bob", points: [1, 2, 1]};
-const player2 = {name: "Andre", points: [2, 0, 1]};
-const player3 = {name: "Max", points: [1, 1, 1]};
-const players = [player1, player2, player3];
-
-console.log("expect 10: ", sumPoints(players));
- 
+const sample = [1,2,3];
+const mapResult = sample.map(function(val, index, array) {
+    console.log('val :', val, 'index :', index, 'array :', array);
+    return (val * 2);
+});
