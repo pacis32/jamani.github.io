59051af93e8617e929b7428016bb93fc6dda1075
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-20 20:42:24 -0500

    update

--- d33closures/closures.js
+++ d33closures/closures.js
@@ -1,7 +1,7 @@
 "use strict";
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
 */
-module.exports = { inArray, inBetween, byField, makeArmy }; //add all of your function names here that you need for the node mocha tests
+//module.exports = { inArray, inBetween, byField, makeArmy }; //add all of your function names here that you need for the node mocha tests
 
 
 /**
