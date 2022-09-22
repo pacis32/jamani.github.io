42ed979d4f7a05340ff10168997e7a68cb126ad2
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-21 20:43:29 -0500

    update

--- d33scheduledCallbacks/playerPoints.js
+++ d33scheduledCallbacks/playerPoints.js
@@ -6,19 +6,42 @@
 */
 module.exports = {findTotalScores, findTotalPlayerPoints }; //add all of your function names here that you need for the node mocha tests
 
+ 
+const player1 = { jersey: 8, stats: [{ game: 1, points: 6 }, { game: 2, points: 7 }] };
+const player2 = { jersey: 12, stats: [{ game: 1, points: 16 }, { game: 2, points: 14 }] };
+const player3 = { jersey: 6, stats: [{ game: 1, points: 10 }, { game: 2, points: 6 }] };
+const teamStats = [player1, player2, player3];
 
 
 /*Be sure to use meaningful variable names and write JSdoc comments for findTotalScores and any helper functions such as findTotalPlayerPoints.
 Try to use map and reduce in your functions as appropriate. */
 
 
-function findTotalScores(teamStatisticsArr) {
+ function findTotalScores(teamStatisticsArr) {
 
+//     const  totalScores=[];
+//     for(let player of teamStatisticsArr){
+//         totalScores.push({jersey: player.jersey, total:findTotalPlayerPoints(player)});
+       
+//     }
 
-}
+// return totalScores;
+
+return teamStatisticsArr.map(player => {
+     return{jersey:player.jersey,  total
+        :  findTotalPlayerPoints(player)}} );
+
+ }
+
 
 
 function findTotalPlayerPoints(player) {
-    
+// let totalPoints=0;
+// for (let game of player.stats) {
+//     totalPoints +=game.points;
+// }
+//return totalPoints
+return player.stats.reduce((sum, curr)=>sum+=curr.points,0)
+;
 
 }
