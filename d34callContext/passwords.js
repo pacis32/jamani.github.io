f07952a9f331d74dae2dec9dca36f6db2b76058a
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-22 20:39:26 -0500

    update

--- d34callContext/passwords.js
+++ d34callContext/passwords.js
@@ -97,7 +97,7 @@ let group2 = {
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
 */
-module.exports = { askPassword, user, askPassword2, user2 }; //add all of your function names here that you need for the node mocha tests
+//module.exports = { askPassword, user, askPassword2, user2 }; //add all of your function names here that you need for the node mocha tests
 
 
 
