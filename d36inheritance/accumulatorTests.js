5158946091a2909de2389490fbf39f8e88bad497
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-09-25 18:14:13 -0500

    update

--- d36inheritance/accumulatorTests.js
+++ d36inheritance/accumulatorTests.js
@@ -1,11 +1,11 @@
 "use strict";
 /* global assert Accumulator */
 // You need the assert and function name declarations to test in node.  
-//Comment these out when you send it to the browser with the index.html mocha setup page.  
+/*Comment these out when you send it to the browser with the index.html mocha setup page.  
 const assert = require("assert");  //always need this with node
 const myExports = require("./accumulator.js");  //with node need the name of your file with your functions here
 const Accumulator = myExports.Accumulator;  //do this for all of the functions used in the Mocha tests
-
+*/
 /*
 Write a constructor function Accumulator(initialValue, increment).  The object it creates should:
 •	Store the current accumulated value in a property currentValue.  The constructor should set this to be initialValue.
