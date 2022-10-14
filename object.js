459b8d15d605e6414b9173bdc8e17b12eceb2ae0
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-10-14 14:21:49 -0500

    up

---
+++ object.js
@@ -0,0 +1,9 @@
+const { white } = require("color-name");
+
+let arrayList = [{name:'rose',color:'pink'},
+ {name:'jasmine',color:'white'},
+ {name:'lily',color:'white'}];
+
+ arrayList.filter( function(obj ,){
+     return item.color==white;
+ })
