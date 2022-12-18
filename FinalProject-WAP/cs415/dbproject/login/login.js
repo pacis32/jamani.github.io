3359352a52ffda9c348454aabf695eea7739eaef
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-12-18 14:50:09 -0600

    update

--- FinalProject-WAP/cs415/dbproject/login/login.js
+++ FinalProject-WAP/cs415/dbproject/login/login.js
@@ -44,7 +44,7 @@ app.post('/auth', function(request, response) {
 				request.session.loggedin = true;
 				request.session.user_id = user_id;
 				// Redirect to home page
-				response.redirect('http://127.0.0.1:5501/FinalProject-WAP/home.html');
+				response.redirect('http://127.0.0.1:5501/FinalProject-WAP/../home.html');
 
                
 			} 
