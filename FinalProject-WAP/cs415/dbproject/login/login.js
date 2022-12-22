dbd3710bba02fa5972fe30b9d429ba91d4a71cd4
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-12-22 10:45:23 -0600

    n

--- FinalProject-WAP/cs415/dbproject/login/login.js
+++ FinalProject-WAP/cs415/dbproject/login/login.js
@@ -53,7 +53,7 @@ app.post('/auth', function(request, response) {
 				request.session.loggedin = true;
 				request.session.user_id = user_id;
 				// Redirect to home page
-				response.redirect('http://localhost:3001/user/register')
+				response.redirect('http://localhost:3005/user/register')
 				
 				}else{
 					request.session.loggedin = true;
