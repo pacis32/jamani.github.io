5f8ec5ceb6ea77707e5a49661d30d020411c0026
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-12-22 12:35:09 -0600

    update

--- FinalProject-WAP/cs415/dbproject/CRUD_MYSQL/server.js
+++ FinalProject-WAP/cs415/dbproject/CRUD_MYSQL/server.js
@@ -1,7 +1,11 @@
 const http =require('http')
 const app= require('./index');
 const port= 3005;
-const server= http.createServer(app);
-server.listen(port,()=>{
-console.log('server listening ')
-});
+// const server= http.createServer(app);
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
